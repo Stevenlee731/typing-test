70669d5f3c84d4c619dbf1f228884dd73c10c321
Stevenlee731 <Stevenlee731@gmail.com>
2017-03-10 14:46:39 -0800

    User can view the letters to type #1

---
+++ main.js
@@ -0,0 +1,8 @@
+/* Users can view the letters to type */
+
+var typing = 'This is a Typing Test'
+
+var $test = document.createElement('h1')
+$test.textContent = typing
+
+document.body.appendChild($test)
